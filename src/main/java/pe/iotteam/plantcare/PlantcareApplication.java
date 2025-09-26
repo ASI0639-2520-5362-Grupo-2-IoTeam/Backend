74974e1a9ee6c1db9ba32de59b741e6188516b6f
AlexlmL <alexcantsed@hotmail.com>
2025-09-25 19:53:11 -0500

    chore: default setup

---
+++ src/main/java/pe/iotteam/plantcare/PlantcareApplication.java
@@ -0,0 +1,13 @@
+package pe.iotteam.plantcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantcareApplication.class, args);
+	}
+
+}
