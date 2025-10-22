2a4ee3004d75a7f73385bc9cd68cda833291565f
AlexlmL <alexcantsed@hotmail.com>
2025-10-21 20:28:05 -0500

    chore: repositories layer

---
+++ src/main/java/pe/iotteam/plantcare/community/infrastructure/persistence/jpa/repositories/PostRepository.java
@@ -0,0 +1,15 @@
+package pe.iotteam.plantcare.community.infrastructure.persistence.jpa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pe.iotteam.plantcare.community.domain.model.entities.Post;
+import pe.iotteam.plantcare.community.domain.model.aggregates.CommunityMember;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, UUID> {
+
+    List<Post> findByAuthor(CommunityMember author);
+}
